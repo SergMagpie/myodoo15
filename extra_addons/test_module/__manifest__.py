64c021b601da35442caabe5563683f5e93efce76
sergmagpie <serhii.sorokin@smartteksas.com>
2023-02-16 13:40:25 +0200

    test module created

--- extra_addons/test_module/__manifest__.py
+++ extra_addons/test_module/__manifest__.py
@@ -5,22 +5,23 @@
     'summary': """
         The module was created by Serhii Sorokin 
         to perform a test task for the position of 
-        Junior Python / Odoo Developer.""",
+        Python / Odoo Developer.""",
 
     'description': """
         The module was created by Serhii Sorokin 
         to perform a test task for the position of 
-        Junior Python / Odoo Developer.
+        Python / Odoo Developer.
     """,
     'author': "Serhii Sorokin",
     'website': "https://portfolio-serhii.herokuapp.com/",
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0.1',
 
-    'depends': [],
+    'depends': [
+        'project',
+    ],
 
     'data': [
-        'security/ir.model.access.csv',
         'views/views.xml',
     ],
     'demo': [],
--- extra_addons/test_module/models/models.py
+++ extra_addons/test_module/models/models.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields
 
 
-class test_module(models.Model):
-    _name = 'test.model'
-    _description = "Test Model"
+class Project(models.Model):
+    _inherit = 'project.project'
 
-    name = fields.Char(required=True)
-    start_date = fields.Date()
-    end_date = fields.Date()
-    duration = fields.Float(compute='_value_pc')
-    tester = fields.Many2one(comodel_name='res.partner')
-
-    @api.depends('start_date', 'end_date')
-    def _value_pc(self):
-        for record in self:
-            if record.end_date and record.start_date:
-                record.duration = (record.end_date - record.start_date).days
-            else:
-                record.duration = 0
+    deparment = fields.Char(
+        string='DEPARTMENT',
+    )
